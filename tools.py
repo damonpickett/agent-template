ab57ba99e4532163ab9324ee27589c2483b123dc
Damon Pickett <damon.pickett@gmail.com>
2026-04-20 17:48:17 -0700

    updated file saving

--- tools.py
+++ tools.py
@@ -1,16 +1,18 @@
 from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain_core.tools import Tool
+from pathlib import Path
 from datetime import datetime
 
 def save_to_txt(data: str, filename: str = "research_output.txt"):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
 
-    with open(filename, "a", encoding="utf-8") as f:
+    output_path = Path(__file__).parent / filename
+    with open(output_path, "a", encoding="utf-8") as f:
         f.write(formatted_text)
     
-    return f"Data successfully saved to {filename}"
+    return f"Data successfully saved to {output_path}"
 
 save_tool = Tool(
     name='save_text_to_file',
@@ -25,5 +27,5 @@ search_tool = Tool(
     description='search the web for info',
 )
 
-api_wrapper = WikipediaAPIWrapper(tok_k_results=1, doc_content_chars_max=100)
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
